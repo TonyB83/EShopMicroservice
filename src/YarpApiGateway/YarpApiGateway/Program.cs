9749061c8d6476b94abfe39ef039f00c389f57d4
TonyB83 <tonyhai83@gmail.com>
2026-03-08 11:11:06 +0100

    Add YarpApi

--- src/YarpApiGateway/YarpApiGateway/Program.cs
+++ src/YarpApiGateway/YarpApiGateway/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.RateLimiting;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+app.UseRateLimiter();
+
+app.MapReverseProxy();
 
 app.Run();
